abaf633326072f54a6ecbbec74d56a7b9d35363d
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2022-08-23 19:01:09 -0400

    Added useEffect with setInterval for timing and Carosel Controls

--- carousel-react-with-time-slider/src/components/Carousel/index.jsx
+++ carousel-react-with-time-slider/src/components/Carousel/index.jsx
@@ -1,14 +1,41 @@
+import { useState, useEffect } from 'react';
 import CarouselItem from "../CarouselItem";
+import CarouselControls from "../../components/CarouselControls";
 import "./styles.css";
 
 export default function Carousel({ slides }) {
+  const [ currSlide, setCurrSlide ] = useState(0);
+
+  
+  // This function will verify if the index of slides is 0, if true, the useEffect setInterval is 5000
+  // function slidesTimer(longer, shorter){
+  //   console.log(longer, shorter)
+
+  // }
+
+  // slidesTimer(5000, 3000)
+  
+  useEffect(()=>{
+    const slideInterval =  setInterval(()=>{
+      setCurrSlide(currSlide => currSlide < slides.length - 1 ? currSlide + 1 : 0);
+    },3000);
+
+    return ()=> clearInterval(slideInterval);
+  },[]);
+
   return (
     <div className="carousel">
-      <div className="carousel-inner">
+      <div 
+    
+        className="carousel-inner"
+        style={{ transform: `translateX(${-currSlide * 100}%)`}}
+        >
         {slides.map((slide, index) => (
           <CarouselItem key={index} slide={slide} />
-        ))}
+          ))}
       </div>
+          <CarouselControls />
+     
     </div>
   );
 }
---
+++ carousel-react-with-time-slider/src/components/CarouselControls/index.jsx
@@ -0,0 +1,9 @@
+import "./styles.css";
+export default function CarouselControls(){
+    return (
+        <div>
+            <button className="carousel-control left">Left</button>
+            <button className="carousel-control right">Right</button>
+        </div>
+    );
+}
